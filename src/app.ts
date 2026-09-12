373530b662d1d7e8cac26b26e3a320f3d8c0a2d7
Hugo Guitti <hguitti@easypost.com>
2026-09-12 16:37:23 -0300

    Separate index file into app and server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { type Express, type Request, type Response } from 'express';
 import "reflect-metadata";
 
+import productsRoutes from "./modules/products/product.routes.js";
+
 
 const app: Express = express();
+
+app.use('/products', productsRoutes)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.listen(5000);
+export default app;
